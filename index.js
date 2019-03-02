5929b3090cda095c3e007e33289271af00b57b01
Dondre Moore <raptureimmortal@icloud.com>
2019-03-02 19:09:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,22 +11,14 @@ function logWhisper(string) {
   console.log('hello'.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string===whisper(string))
-  return ("I can't hear you!")
-  }
-  function sayHiToGrandma(string) {
-    if (string===shout(string))
-  return ("YES INDEED!") 
-}
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if (string.toLowerCase() === string)
     return "I can't hear you!"
   }
-
-  if (string.toUpperCase() === string) {
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string)
     return "YES INDEED!"
   }
-
-  if (string === "I love you, Grandma.") {
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") 
     return "I love you, too."
   }
