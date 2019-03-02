9110ce4c04197e731ff52b3343778ff493712eb8
Dondre Moore <raptureimmortal@icloud.com>
2019-03-02 19:03:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function sayHiToGrandma(string) {
   return ("I can't hear you!")
   }
   function sayHiToGrandma(string) {
-    if (string===shout(string))
+    else if (string===shout(string))
   return ("YES INDEED!") 
 }
