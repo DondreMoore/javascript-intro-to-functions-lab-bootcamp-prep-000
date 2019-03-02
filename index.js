7a42912b0afbcaa84ea17cafeeba264f28693d35
Dondre Moore <raptureimmortal@icloud.com>
2019-03-02 19:33:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,18 +10,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello'.toLowerCase())
 }
-function sayHiToGrandma(string) {
-  if (string === toLowerCase()) {
-  return ("I can't hear you!");
-}
-
-
-if (string === toUpperCase())
-return ("YES INDEED!");
-}
-
-  
-  if (string === "I love you, Grandma.")
-  return ("I love you, too.");
-  }
-}
